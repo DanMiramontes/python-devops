11e0d1fcaabee70bbd95b384134c845652eac467
DanMiramontes <dmiramontesg@gmail.com>
2023-07-10 19:17:47 -0600

    new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,7 @@ def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+def search_wiki(name):
+    """Search wikipedia for names"""
+    result = wikipedia.search(name)
+    return result
